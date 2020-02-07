f3017580190b5431b1d06c6f4912c257e2af792d
WShell Surface <washellington@gmail.com>
2020-02-07 16:22:01 -0500

    added rest of REST api calls to controller

--- controllers/mental_state.controller.js
+++ controllers/mental_state.controller.js
@@ -4,6 +4,8 @@ import MentalState from "../models/mental_state.model";
 const uri =
   "mongodb+srv://moodyApi:nsusga06@cluster0-b4mio.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
+const COLLECTION = "MentalState";
+
 class MentalStateController {
   static test(req, res) {
     res.send("Greetings from the Test controller!");
@@ -15,26 +17,87 @@ class MentalStateController {
     await client.connect();
     console.log("connected");
     const db = client.db("moodyDb");
-    let mental_state = {
+    let model = {
       rating: parseInt(req.body.rating),
       mood_type: parseInt(req.body.mood_type_id),
       date_created: new Date().getTime(),
       user: parseInt(req.body.user_id),
       notes: req.body.notes
     };
-    console.log(mental_state);
-    db.collection("MentalState").insertOne(mental_state, (err, result) => {
+    console.log(model);
+    db.collection(COLLECTION).insertOne(model, (err, result) => {
       if (err) throw err;
       console.log("results are = ", result);
       res.sendStatus(200);
     });
   }
+  static async read(req, res) {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-  static read(req, res) {}
+    try {
+      await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    const db = client.db("moodyDb");
+    let id = req.params.id;
 
-  static update(req, res) {}
+    let user = await db.collection(COLLECTION).findOne({ _id: ObjectId(id) });
 
-  static delete(req, res) {}
+    if (user) {
+      console.log("found mental_state:", user);
+    } else {
+      console.warn("could not find user with id: ", id);
+    }
+    res.sendStatus(200);
+  }
+
+  static async update(req, res) {
+    let model = {
+      rating: parseInt(req.body.rating),
+      mood_type: parseInt(req.body.mood_type_id),
+      date_created: new Date().getTime(),
+      user: parseInt(req.body.user_id),
+      notes: req.body.notes
+    };
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+      await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    const db = client.db("moodyDb");
+    let id = req.params.id;
+
+    let results = await db
+      .collection(COLLECTION)
+      .updateOne({ _id: ObjectId(id) }, { $set: model });
+
+    console.log("updated mental_state result :", results);
+
+    res.sendStatus(200);
+  }
+
+  static async delete(req, res) {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+      await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    const db = client.db("moodyDb");
+    let id = req.params.id;
+
+    let results = await db
+      .collection(COLLECTION)
+      .deleteOne({ _id: ObjectId(id) });
+
+    console.log("updated user result :", results);
+
+    res.sendStatus(200);
+  }
 }
 
 export default MentalStateController;
